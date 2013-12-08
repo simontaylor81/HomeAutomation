9942f102b47ee476a50b8fdabd780f98a4a46f4a
Simon Taylor <simontaylor81@hotmail.com>
2013-12-08 21:23:52 +0000

    Switched to bootstrap instead of jquery-mobile

--- MasterServer/public/javascripts/main.js
+++ MasterServer/public/javascripts/main.js
@@ -8,7 +8,7 @@ var xbmcUrl = mediaCentreServerUrl + "/api/xbmcjsonrpc";
 // Disable caching of ajax requests.
 $.ajaxSetup({ cache: true });
 
-$(document).bind('pageinit', function () {
+$(document).ready(function(){
     // Set server name in header.
     $('#serverName').html(serverName);
 
@@ -26,58 +26,53 @@ $(document).bind('pageinit', function () {
 });
 
 function setPowerStatus(status) {
-    setStatus(status, 'Power', '#powerButton', turnOn, turnOff, true);
+    setStatus(status, '#power-panel', {
+        On: turnOff,
+        Off: turnOn
+    });
 }
 function setXbmcStatus(status) {
-    setStatus(status, 'XBMC', '#xbmcButton', startXbmc, closeXbmc, false);
+    setStatus(status, '#xbmc-panel', {
+        On: closeXbmc,
+        Off: startXbmc
+    });
 }
 
-function setStatus(status, prefix, buttonId, onFunction, offFunction, setHeader) {
-    // Set header theme.
-    themes = {
-        On: 'f',
-        Off: 'g',
-        Pending: 'h'
+function setStatus(status, panelSelector, actions) {
+    classes = {
+        On: 'ha-green',
+        Off: 'ha-red',
+        Pending: 'ha-amber',
     };
-    var theme = themes[status];
+    var panelClass = classes[status];
 
-    // Set on-off button to do the right thing.
-    var button = $(buttonId);
-    var text = prefix + ': ' + status;
-    var disabled = false;
-    var onClick = false;
+    // Set class of panel.
+    var panel = $(panelSelector);
+    panel.removeClass('ha-green ha-red ha-amber').addClass(panelClass);
 
-    switch (status) {
-        case 'Off':
-            onClick = onFunction;
-            break;
+    // Set status text.
+    panel.find('.ha-status-text').html(status);
 
-        case 'On':
-            onClick = offFunction;
-            break;
-
-        case 'Pending':
-            disabled = true;      // Disable if we're pending.
-            break;
+    // Set state of buttons in the panel.
+    var buttons = panel.find('button');
+    if (status === 'On') {
+        buttons.addClass('ha-on');
+    } else {
+        buttons.removeClass('ha-on');
     }
 
-    // Workaround for jqm bug: set data-theme on button wrapper div too.
-    button.parent().attr('data-theme', theme);
+    var buttonText = {
+        On: 'Turn Off',
+        Off: 'Turn On',
+        Pending: 'Pending'
+    }[status];
+    buttons.find('.ha-button-text').html(buttonText);
 
-    button
-        .html(text)
+    // Set click function to appropriate action.
+    buttons
         .off('click')
-        .on('click', onClick)
-        .attr('data-theme', theme)
-        .buttonMarkup({ theme: theme })
-        .button(disabled ? 'disable' : 'enable')
-        .button('refresh')
-    ;
-
-    // Set status text in header.
-    if (setHeader) {
-        setTheme($("#header"), 'bar', theme);
-    }
+        .on('click', actions[status])
+        .prop('disabled', status === 'Pending');
 }
 
 function turnOn() {
@@ -193,25 +188,20 @@ function startXbmc() {
     });
 }
 
-// Helper for setting theme on things that don't expose the required methods.
-function setTheme(elem, type, theme) {
-    // Remove existing theme classes.
-    elem.removeClass(function (index, prevClass) {
-        var allClasses = prevClass.split(' ');
-        var toRemove = allClasses.filter(function (c) { return c.indexOf('ui-' + type + '-') >= 0; });
-        return toRemove.join(' ');
-    });
-
-    // Add the new class.
-    elem.addClass('ui-' + type + '-' + theme);
-}
-
 // Quick testing.
 var statusCycle = 0;
 function testStatus() {
     var status = ['Off', 'On', 'Pending'][statusCycle];
     setPowerStatus(status);
     statusCycle = (statusCycle + 1) % 3;
+}
+
+function testOnOff() {
+    $('.ha-btn-power').toggleClass('ha-on');
+
+    var panel = $('#power-panel');
+    var newClass = panel.hasClass('ha-green') ? 'ha-red' : 'ha-green';
+    panel.removeClass('ha-green ha-red').addClass(newClass);
 }
 
 // Add a log message.
