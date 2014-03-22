22efb2393537e9faaf7aa29b72f57ee7e12d2422
Simon Taylor <simontaylor81@hotmail.com>
2014-03-22 17:21:24 +0000

    Added some space between widgets
    So they don't look so cramped. Also tidied up a few other minor
    formatting things.

--- MasterServer/public/views/widgets/textView.js
+++ MasterServer/public/views/widgets/textView.js
@@ -6,11 +6,11 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += " <div class=\"input-group\">\r\n    <input type=\"text\" class=\"form-control\" value=\"";
+  buffer += "<div class=\"input-group\">\r\n    <input type=\"text\" class=\"form-control\" value=\"";
   if (stack1 = helpers.initialText) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.initialText); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\">\r\n    <span class=\"input-group-btn\">\r\n        <button class=\"btn btn-default\" type=\"button\">Go</button>\r\n    </span>\r\n</div>";
+    + "\">\r\n    <span class=\"input-group-btn\">\r\n        <button class=\"btn btn-default\" type=\"button\">Go</button>\r\n    </span>\r\n</div>\r\n";
   return buffer;
   })
 
