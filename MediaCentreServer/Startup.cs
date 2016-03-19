048c922f1884082a2a00915ecfec915fd41fac35
Simon Taylor <simontaylor81@hotmail.com>
2016-03-19 11:38:48 +0100

    Set up basic logging

--- MediaCentreServer/Startup.cs
+++ MediaCentreServer/Startup.cs
@@ -1,27 +1,29 @@
 using System.Web.Http;
-using System.Web.Http.Cors;
 using Owin;
+using System.Web.Http.Tracing;
 
 namespace MediaCentreServer
 {
-    public class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-			app.UseErrorPage();
-
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
 			// Enable CORS (fully open -- it's internal only).
 			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
 
-            // Configure Web API for self-host.
-            var config = new HttpConfiguration();
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+			// Configure Web API for self-host.
+			var config = new HttpConfiguration();
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			// Configure logging.
+			var traceWriter = config.EnableSystemDiagnosticsTracing();
+			traceWriter.MinimumLevel = TraceLevel.Warn;
 
 			app.UseWebApi(config);
-        }
-    }
+		}
+	}
 }
